7ebcbd97767ca9e75340b77959b430d1faddec6b
akash-meher-78 <akashmeher6635@gmail.com>
2025-08-15 01:41:30 +0530

    Add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,50 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Product from "./pages/Product"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Cart from "./pages/Cart"
+import Navbar from "./components/Navbar"
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 function App() {
 
+  const [localtions, setloactions] = useState()
+
+  const location  = async() =>{
+    navigator.geolocation.getCurrentPosition(async pos => {
+      const {latitude, longitude} = pos.coords
+      console.log(latitude, longitude);
+
+      const url =  `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`
+
+      try {
+        const location = await axios.get(url)
+        const exacrLocation = location.data.address
+        setloactions(exacrLocation)
+        
+      } catch (error) {
+        console.log(error);
+      }
+    })
+  }
+
+  useEffect(() =>{
+    location()
+  }, [])
+
   return (
-    <>
-      <h1 className="h-screen w-full bg-black text-white  text-center font-monotext-4xl"></h1>
-    </>
+    <BrowserRouter>
+    <Navbar localtions={localtions}/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/products" element={<Product/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/cart" element={<Cart/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
